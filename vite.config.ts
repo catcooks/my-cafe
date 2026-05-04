9020d5049c857a04b85632363143051db49a4366
CatCooks <javerbenito@gmail.com>
2026-05-04 13:18:59 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Since you're using a custom domain at the root, use '/'
+  base: '/', 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
